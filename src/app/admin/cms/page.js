efc979b91a7aea0f5efa3818da4b348b8bd349bc
sai kiran <pulakhanamsaikiran834@.com>
2026-02-18 17:56:57 +0530

    hello

--- src/app/admin/cms/page.js
+++ src/app/admin/cms/page.js
@@ -3,7 +3,7 @@ export default function page() {
     return (
         <div style={{ marginTop: "15px", marginLeft: "80px" }}>
             <h1>
-                this is a cms page
+                this is cms page
             </h1>
         </div>
     )
